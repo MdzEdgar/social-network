1a2f7c2c9fa3ceb04aa7ffc7430dd9532530498a
Edgar Méndez <mdzredgar@gmail.com>
2023-04-24 21:03:16 -0500

    Correción en users.services

--- src/users/users.services.js
+++ src/users/users.services.js
@@ -4,7 +4,7 @@ const userControllers = require('./users.controllers')
 const getAllUsers = (req, res) => {
     userControllers.findAllUsers()
         .then((data) => {
-            res.status(200).json({message: `El usuario ${req.user.firstName} hizo esta peticion`, data})
+            res.status(200).json(data)
         })
         .catch((err) => {
             res.status(400).json({message: 'Bad request', err})
