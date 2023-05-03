f845e227b892b27ab4d18f5959529aae5ba307f7
Edgar Méndez <mdzredgar@gmail.com>
2023-05-02 21:38:14 -0500

    Integracion de multer. Agregadas rutas al app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const cors = require('cors')
+const path = require('path')
 require('dotenv').config()
 
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
 const postsRouter = require('./posts/posts.router')
+const followRouter = require('./follows/follows.router')
 
+const upload = require('./middlewares/multer.middleware')
 const db = require('./utils/database')
 const initModels = require('./models/initModels')
 const app = express()
@@ -29,11 +32,7 @@ app.use(cors())
 
 const loggerMiddleware = (req, res, next) => {
     console.log(`${req.method} | ${req.path}`)
-    if(req.method !== 'DELETE') {
-        next()
-        return
-    }
-    res.status(400).json({message: 'No hagas eso papu :c'})
+    next()
 }
 app.use(loggerMiddleware)
 
@@ -44,11 +43,23 @@ app.get('/', (req, res) => {
         myPort: process.env.PORT,
         queries: req.query
     })
-}) 
+})
+
+app.get('/api/v1/uploads/:fileName', (req, res) => {
+    //const fileName = req.params.fileName
+    res.status(200).sendFile(path.resolve('./public/') + '/' + req.params.fileName)
+})
+
+app.post('/api/v1/add-file', upload.single('my-image') ,(req, res) => {
+
+    res.status(200).json({message: req.file})
+})
+
 
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/posts', postsRouter)
+app.use('/api/v1', followRouter)
 
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`)
